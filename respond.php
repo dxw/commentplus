78a7299bf3084730a0c7c71138457583f8194c7d
Tom Adams <tom@holizz.com>
2010-02-15 16:24:16 +0000

    Fix tabindex.

--- respond.php
+++ respond.php
@@ -22,22 +22,22 @@ if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['
 
 <?php else : ?>
 
-<p><input type="text" name="author" id="author_<?php echo $n ?>" value="<?php echo esc_attr($comment_author); ?>" size="22" tabindex="1" <?php if ($req) echo "aria-required='true'"; ?> />
+<p><input type="text" name="author" id="author_<?php echo $n ?>" value="<?php echo esc_attr($comment_author); ?>" size="22" tabindex="<?php echo $n ?>1" <?php if ($req) echo "aria-required='true'"; ?> />
 <label for="author_<?php echo$n ?>"><small>Name <?php if ($req) echo "(required)"; ?></small></label></p>
 
-<p><input type="text" name="email" id="email_<?php echo $n ?>" value="<?php echo esc_attr($comment_author_email); ?>" size="22" tabindex="2" <?php if ($req) echo "aria-required='true'"; ?> />
+<p><input type="text" name="email" id="email_<?php echo $n ?>" value="<?php echo esc_attr($comment_author_email); ?>" size="22" tabindex="<?php echo $n ?>2" <?php if ($req) echo "aria-required='true'"; ?> />
 <label for="email_<?php echo$n ?>"><small>Mail (will not be published) <?php if ($req) echo "(required)"; ?></small></label></p>
 
-<p><input type="text" name="url" id="url_<?php echo $n ?>" value="<?php echo esc_attr($comment_author_url); ?>" size="22" tabindex="3" />
+<p><input type="text" name="url" id="url_<?php echo $n ?>" value="<?php echo esc_attr($comment_author_url); ?>" size="22" tabindex="<?php echo $n ?>3" />
 <label for="url_<?php echo$n ?>"><small>Website</small></label></p>
 
 <?php endif; ?>
 
 <!--<p><small><strong>XHTML:</strong> You can use these tags: <code><?php echo allowed_tags(); ?></code></small></p>-->
 
-<p><textarea name="comment" id="comment_<?php echo $n ?>" cols="58" rows="10" tabindex="4"></textarea></p>
+<p><textarea name="comment" id="comment_<?php echo $n ?>" cols="58" rows="10" tabindex="<?php echo $n ?>4"></textarea></p>
 
-<p><input name="submit" type="submit" id="submit_<?php echo $n ?>" tabindex="5" value="Submit Comment" />
+<p><input name="submit" type="submit" id="submit_<?php echo $n ?>" tabindex="<?php echo $n ?>5" value="Submit Comment" />
 <?php comment_id_fields(); ?>
 </p>
 <?php do_action('comment_form', $post->ID); ?>
