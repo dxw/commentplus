16ee51739f3605becde7b6562f77bd8790ce05c6
Tom Adams <tom@holizz.com>
2010-02-15 15:41:54 +0000

    OOPify.

--- commentplus.php
+++ commentplus.php
@@ -6,20 +6,25 @@ Author: The Dextrous Web
 Author URI: http://thedextrousweb.com/
 */
 
-function commentplus_comments_template($template) {
-  return dirname(__FILE__).'/comments.php';
+class CommentPlus {
+  function __construct() {
+    add_filter('comments_template', array(&$this, 'comments_template'));
+    add_action('comment_post', array(&$this, 'comment_post'));
+  }
+
+  function comments_template($template) {
+    return dirname(__FILE__).'/comments.php';
+  }
+
+  function comment_post($comment_ID) {
+    $comment = get_comment($comment_ID);
+    $stream = $_POST['commentplus_stream'];
+    $streams = json_decode(get_post_meta($comment->comment_post_ID, 'commentplus', 1));
+    if ($streams && in_array($stream, $streams))
+      add_comment_meta($comment_ID, 'commentplus_stream', $stream);
+  }
 }
 
-add_filter('comments_template', 'commentplus_comments_template');
-
-function commentplus_comment_post($comment_ID) {
-  $comment = get_comment($comment_ID);
-  $stream = $_POST['commentplus_stream'];
-  $streams = json_decode(get_post_meta($comment->comment_post_ID, 'commentplus', 1));
-  if ($streams && in_array($stream, $streams))
-    add_comment_meta($comment_ID, 'commentplus_stream', $stream);
-}
-
-add_action('comment_post', 'commentplus_comment_post');
+new CommentPlus;
 
 ?>
