2000bf3bb7c3749569efce080e5495867144d7ea
Tom Adams <tom@holizz.com>
2010-02-15 11:43:15 +0000

    Take security header from default/comments.php.

--- comments.php
+++ comments.php
@@ -1,4 +1,13 @@
 <?php
+if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
+  die ('Please do not load this page directly. Thanks!');
+
+if ( post_password_required() ) {
+  echo '<p class="nocomments">This post is password protected. Enter the password to view comments.</p>';
+  return;
+}
+?>
+<?php
 $streams = json_decode(get_post_meta($post->ID, 'commentplus',1));
 
 if (comments_open() || have_comments()) {
