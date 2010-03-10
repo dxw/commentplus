e7b6d1d93db6927282554f1e99e51f131d4d9417
Tom Adams <tom@holizz.com>
2010-03-10 09:51:43 +0000

    Don't do _anything_ without the parameters in get_comments.

--- get_comments.php
+++ get_comments.php
@@ -3,17 +3,17 @@ if (isset($_GET['post']) && isset($_GET['stream']) && isset($_GET['cpage'])) {
   $post_ID = (int)$_GET['post'];
   $n = (int)$_GET['stream'];
   $cpage = (int)$_GET['cpage'];
-}
-require(dirname(__FILE__).'/../../../wp-load.php');
-query_posts(array('p'=>$post_ID, 'post_type'=>'any'));
-if (!have_posts())
-  return;
-global $commentplus;
-the_post();
+  require(dirname(__FILE__).'/../../../wp-load.php');
+  query_posts(array('p'=>$post_ID, 'post_type'=>'any'));
+  if (!have_posts())
+    return;
+  global $commentplus;
+  the_post();
 
-if(!$commentplus->has_streams($n))
-  return;
-set_query_var('cpage', $cpage);
-$commentplus->init_ajah();
-comments_template('/commentplus_ajah');
+  if(!$commentplus->has_streams($n))
+    return;
+  set_query_var('cpage', $cpage);
+  $commentplus->init_ajah();
+  comments_template('/commentplus_ajah');
+}
 ?>
